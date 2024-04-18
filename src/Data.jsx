09486106b58e1c5a2ce8d661ab3bcd218457e0bb
SrulySteiner <134973559+SrulySteiner@users.noreply.github.com>
2024-04-18 09:47:09 -0400

    Seder Calculator Logic updated
    Almost finished Seder Calculator logic, still needs to be modified to accurately reflect the current index of that day's Seder, and bugs of miscalculating endDate/totals

--- src/Data.jsx
+++ src/Data.jsx
@@ -10,9 +10,11 @@ function dataComp({d, t, s}) {
   const [text, setText] = useState('Genesis');
   const [index, setIndex] = useState([]);
   const [length, setLength] = useState([]);
-  const [studyType, setStudyType] = useState();
-  const [startDate, setStartDate] = useState(d.currentDate);
-  const [endDate, setEndDate] = useState();
+  const [studyType, setStudyType] = useState('Date');
+  const [metric, setMetric] = useState();
+  const [increment, setIncrement] = useState();
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
   const changeCategory = (event) => {
     setCategory(event.target.value);
@@ -23,7 +25,25 @@ function dataComp({d, t, s}) {
   }
 
   const changeStudyType = (event) =>{
-    setStudyType(event.target.value);
+    setStudyType(event.target.value.replace('By', '').trim());
+    setMetric(event.target.value.replace('By', '').trim());
+  }
+
+  const changeIncrement = (event) =>{
+    if(studyType == 'Date'){
+      let daysBetween = Math.round((endDate - startDate) / (1000 * 60 * 60 * 24)) + 1;
+      setMetric(event.target.value);
+      let totalLength = metric == index[0] ? length.length : length.reduce((a,b)=>a+b);
+      let increment = Math.round(totalLength / daysBetween);
+      console.log(increment);
+      console.log(startDate);
+      console.log(endDate);
+      setIncrement(increment);
+    }else if(studyType == index[0]){
+      
+    }else{
+
+    }
   }
 
 
@@ -37,16 +57,24 @@ function dataComp({d, t, s}) {
       };
       s.setSeder(() => {
         const newSedarim = [...s.sedarim, newSeder];
-        console.log(newSedarim);
+        //console.log(newSedarim);
         localStorage.setItem("sedarim", JSON.stringify(newSedarim));
         return newSedarim;
       });
       let loop = new Date(startDate);
       let newTasks = [...t.tasks];
+      /*TODO: endDate, and therefore daysBetween, calculated based on study type - 
+        if user selected 4 chapters a day, calculates the endDate for them
+      */
+      let totalLength = metric == index[0] ? length.length : length.reduce((a,b)=>a+b);
+      console.log(totalLength);
+      let dayStart = 1;
+      let dayEnd = dayStart + increment - 1;
+      totalLength -= increment;
       while (loop.getDate() <= endDate.getDate()) {
         let newTask = {
           id: crypto.randomUUID(),
-          name: newSeder.name,
+          name: newSeder.name + ' ' + metric + ' ' + dayStart + '-' + dayEnd,
           date: loop.toLocaleDateString(),
           isSeder: true,
           sederId: newSeder.id
@@ -54,6 +82,9 @@ function dataComp({d, t, s}) {
         newTasks = [...newTasks, newTask];
         let newDate = loop.setDate(loop.getDate() + 1);
         loop = new Date(newDate);
+        dayStart = dayEnd + 1;
+        dayEnd = totalLength < increment ? dayEnd + totalLength : dayEnd + increment;
+        totalLength = totalLength > increment ? totalLength - increment : 0;
       }
       t.setTasks(() => {
         localStorage.setItem("tasks", JSON.stringify(newTasks));
@@ -87,7 +118,8 @@ function dataComp({d, t, s}) {
         const response = await fetch(address);
         const data = await response.json();
         setIndex(data.sectionNames);
-        console.log(index);
+        setMetric(index[0]);
+        //console.log(index);
         setLoading(false);
       } catch (error) {
         setError(error);
@@ -104,7 +136,7 @@ function dataComp({d, t, s}) {
         const response = await fetch(address);
         const data = await response.json();
         setLength(data[0].chapters);
-        console.log(length);
+        //console.log(length);
         setLoading(false);
       } catch (error) {
         setError(error);
@@ -112,7 +144,7 @@ function dataComp({d, t, s}) {
       }
     }
     fetchLength();
-  }, [text, index]);
+  }, [category, text, index]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -146,31 +178,49 @@ function dataComp({d, t, s}) {
 
     <label>Pick a Study Type:
       <select onChange={changeStudyType}>
+      <option key={'byDate'}> 
+          By Date
+        </option>
         {index.map((i) =>{
           return <option key={i}>
                   By {i}
                  </option>
         })}
-        <option key={'byDate'}> 
-          By Date
-        </option>
       </select>
     </label>
+    {(studyType == 'Date') ? 
+      <div>
+        <label > Pick Start and End:
+          <DatePicker
+            selectsStart
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            startDate={startDate}/>
+          <DatePicker
+            selectsEnd
+            selected={endDate}
+            onChange={(date) => setEndDate(date)}
+            endDate={endDate}
+            startDate={startDate}
+            minDate={startDate}/>
+        </label>
+        <label> Based on:
+          <select onChange={changeIncrement}>
+            {index.map((i) =>{
+              return <option key={i}>
+                      {i}
+                    </option>
+            })}
+          </select>       
+        </label>
+      </div>
+       
+     :
+      <label>How many per day?
+        <input type="number" id="metric" name="metric" min="1" max={length.length} onChange={changeIncrement} />
+      </label>  
+    }
 
-    <DatePicker
-        selectsStart
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
-        startDate={startDate}
-      />
-      <DatePicker
-        selectsEnd
-        selected={endDate}
-        onChange={(date) => setEndDate(date)}
-        endDate={endDate}
-        startDate={startDate}
-        minDate={startDate}
-      />
       <button onClick={() => createSeder()}>Create Seder</button>
    </div>
     );
